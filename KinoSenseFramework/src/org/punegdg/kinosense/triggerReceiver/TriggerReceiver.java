89da7693720f759abddb89a905ef8cbf5584da47
Rohit Ghatol <rohit@mac-pro60.local>
2012-08-11 00:19:36 +0530

    First Commit for the project - Commiting a Template Android Project with proper code formatting and some working triggers and actions

---
+++ KinoSenseFramework/src/org/punegdg/kinosense/triggerReceiver/TriggerReceiver.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2012 Pune-GDG (http://meetup.com/pune-gdg)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.punegdg.kinosense.triggerReceiver;
+
+import org.punegdg.kinosense.actions.BaseAction;
+import org.punegdg.kinosense.actions.SilentAction;
+import org.punegdg.kinosense.actions.VibrateAction;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Receives all the Kino Sense Triggers and runs the rules to invoke the
+ * corresponding actions
+ * 
+ * @author "Rohit Ghatol" <rohitsghatol@gmail.com>
+ * 
+ */
+public class TriggerReceiver extends BroadcastReceiver {
+	public static String ACTION_KINOSENSE_TRIGGER = "org.punegdg.kinosense.TRIGGER";
+
+	/** 
+	 * Silent Action 
+	 */
+	private BaseAction silentAction = new SilentAction();
+	
+	/**
+	 * Vibrate Action
+	 */
+	private BaseAction vibrateAction = new VibrateAction();
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.content.BroadcastReceiver#onReceive(android.content.Context,
+	 * android.content.Intent)
+	 */
+	@Override
+	public void onReceive(Context context, Intent intent) {
+
+		// -----------------------------
+		// FIXME Fix the following
+		// -----------------------------
+		silentAction.onCreate(context);
+		vibrateAction.onCreate(context);
+
+		// -----------------------------
+
+		String trigger = intent.getStringExtra("trigger");
+		if ("FLIPPED_DOWN".equals(trigger)) {
+			vibrateAction.perform(null, null);
+			silentAction.perform("Silence", null);
+		} else if ("FLIPPED_UP".equals(trigger)) {
+			vibrateAction.perform(null, null);
+			silentAction.perform("Restore", null);
+		}
+	}
+
+}
--- README.md
+++ README.md
@@ -2,3 +2,39 @@ KinoSense
 =========
 
 Enabling Smart Phones making Sense by itself
+
+Terminologies
+===========
+
+We need to understand the following terminologies
+
+* Triggers
+* Actions
+* Rules
+* Profiles
+
+Triggers
+----
+Triggers can be both low level or high level. An Accelerometer event can be a low level trigger. A User flipping the phone facedown can be a high level triggers. Triggers are to be used alone or in conjuction and actions needs to be fired when triggers fulfill a set of rules
+
+Actions
+----
+Actions can be low level or high level. Phone going on Silent mode can be low level action. A User phone is silent mode, but phone calls answered by auto reply SMS "Busy in meeting" can be considered a high level "Do not Distrub" Action
+
+Rules
+----
+Rules is nothing but fulfillment of set of Triggers (and may be some context). If rules are fulfilled appropriate actions should be fired
+
+Profiles
+----
+Profiles are templates which are in term set of rules. A User can create various profiles or clone some well known profiles. This way user does not have to manage individual rules
+
+
+Project Members
+----
+* Rohit Ghatol
+* Hrishikesh Bhruguwar
+* Ashish Kalbhor
+* Sandeep Mane
+* Apurva Bhoite
+* Amruta Deshpande

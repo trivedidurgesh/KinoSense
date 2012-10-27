e0b99c2837d67f0d643a3cc379631d2ff36d2017
Ashish Kalbhor <ashish.kalbhor@gmail.com>
2012-10-27 11:58:31 +0530

    Flight Mode Action Added

--- KinoSenseFramework/src/org/punegdg/kinosense/triggerReceiver/TriggerReceiver.java
+++ KinoSenseFramework/src/org/punegdg/kinosense/triggerReceiver/TriggerReceiver.java
@@ -16,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.punegdg.kinosense.actions.AbstractAction;
+import org.punegdg.kinosense.actions.FlightModeAction;
 import org.punegdg.kinosense.actions.SilentAction;
 import org.punegdg.kinosense.actions.VibrateAction;
 import org.punegdg.kinosense.actions.WifiAction;
@@ -50,6 +51,11 @@ public class TriggerReceiver extends BroadcastReceiver
 
 	private AbstractAction wifiAction = new WifiAction();
 
+	/**
+	 * Filght Mode Action
+	 */
+	private AbstractAction flightaction = new FlightModeAction();
+
 
 	/*
 	 * (non-Javadoc)
@@ -65,6 +71,7 @@ public class TriggerReceiver extends BroadcastReceiver
 		this.silentAction.onCreate(context);
 		this.vibrateAction.onCreate(context);
 		this.wifiAction.onCreate(context);
+		this.flightaction.onCreate(context);
 		// -----------------------------
 
 		String trigger = intent.getStringExtra("trigger");
@@ -91,12 +98,12 @@ public class TriggerReceiver extends BroadcastReceiver
 		if ( "POWER_CONNECTED".equals(trigger) )
 		{
 			this.vibrateAction.perform(null);
-
+			this.flightaction.perform(null);
 		}
 		else if ( "POWER_DISCONNECTED".equals(trigger) )
 		{
 			this.vibrateAction.perform(null);
-
+			this.flightaction.onDestroy();
 		}
 	}
 

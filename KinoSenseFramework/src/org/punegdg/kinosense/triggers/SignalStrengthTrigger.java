3e51bb832518c4749f90c8ba66d4eb480394e7d0
Amruta Deshpande <deshpande.amruta22@gmail.com>
2013-03-18 01:23:43 +0530

    signal Strength
    To get notification when cellular signal strength is below predefined
    range

--- KinoSenseFramework/src/org/punegdg/kinosense/triggers/SignalStrengthTrigger.java
+++ KinoSenseFramework/src/org/punegdg/kinosense/triggers/SignalStrengthTrigger.java
@@ -1,3 +1,19 @@
+
+/**
+ * Copyright 2012 Pune-GDG (http://meetup.com/pune-gdg)
+
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+
 package org.punegdg.kinosense.triggers;
 
 import org.punegdg.kinosense.triggerReceiver.TriggerReceiver;
@@ -13,6 +29,14 @@ import android.telephony.SignalStrength;
 import android.telephony.TelephonyManager;
 
 import android.widget.Toast;
+
+
+/**
+ * This Trigger is for the action when Cellular Signal Strength is below predefined range
+ * 
+ * @author "Amruta Deshpande"<deshpande.amruta22@gmail.com>
+ * 
+ */
 
 public class SignalStrengthTrigger extends BroadcastReceiver implements BroadCastReceiverBasedTrigger
 {
